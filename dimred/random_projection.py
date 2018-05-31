74f662d8f3685d208f16938796876dadceec6b65
Mohammadsadegh Akhondzadeh <ms.akhondzadeh@gmail.com>
2018-05-31 15:36:05 +0430

    add random projection

--- dimred/random_projection.py
+++ dimred/random_projection.py
@@ -1,26 +1,36 @@
 import numpy as np
-import pandas as pd
 
 class RandomProjection():
 
 
     def __init__(self, n_component):
         self.ncomponent = n_component
-        self.tarnsform_matrix = None
+        self.component = None
+        self.nfeatures = None
 
 
 
     def _fit(self, X):
-
+        self.nfeatures = X.shape[1]
+        r = np.random.mtrand._rand
+        self.component = r.normal(loc = 0,
+                                    scale = 1 / np.sqrt(self.ncomponent),
+                                    size = (self.ncomponent, self.nfeatures ))
 
 
     def fit(self, X):
+        return self._fit(X)
 
 
 
 
 
     def transform(self, X):
+
+        X_new = np.dot(X, self.component)
+        return X_new
+
+
 
 
 
