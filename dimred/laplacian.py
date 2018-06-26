558198e7db8cfd0d06f09db687ec780367fd33a0
Mohammadsadegh Akhondzadeh <ms.akhondzadeh@gmail.com>
2018-06-26 15:37:45 +0430

    add spectral embedding

--- dimred/laplacian.py
+++ dimred/laplacian.py
@@ -1,31 +1,42 @@
 from sklearn.neighbors import kneighbors_graph
 from sklearn.neighbors import NearestNeighbors
-from sklearn.utils.graph import graph_shortest_path
+from scipy.sparse.linalg import eigsh
+from scipy import sparse
 import numpy as np
-import scipy as sp
 
 
 class Laplacian():
 
 
-    def __init__(self, n_component = 2, method, gamma, n_neighbours):
-        self.n_neighbours = None
-        self.method = None
+    def __init__(self, n_neighbours =50, n_component = 2, gamma= None, method ='kneighbour'):
+        self.n_neighbour = n_neighbours
         self.n_component = n_component
-        self.gamma = None
+        self.method = method
+        self.gamma = gamma
         self.affinity = None
         self.kneargraph = None
         self.output = None
 
 
+
     def _fit(self, X):
+        self.affinity = self.affinity_matrix(X)
 
 
-        affinity = affinity_matrix(X)
+        #
+        laplacian, dig = sparse.csgraph.laplacian(self.affinity, normed=True, return_diag=True)
+        laplacian *= -1
+        lambdas, eigvec = eigsh(laplacian,sigma = 1.0, k=self.n_component + 1, which='LM')
+        self.output = eigvec.T[self.n_component+1 :: -1] * dig
+        self.output = self.output[1:self.n_component + 1].T
 
-
+        return self
 
     def fit(self, X):
+
+        opj = self._fit(X)
+
+        return self
 
 
 
@@ -42,6 +53,15 @@ class Laplacian():
 
 
 
-    def transform(self, X):
-        pass
+    def transform(self):
+        return self.output
+
+
+
+    def fit_transform(self, X):
+
+
+        obj = self._fit(X)
+        return self.output
+
 
